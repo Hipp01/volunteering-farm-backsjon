bc095c2c0221a4052f2525c019188ce1cd0661f1
Hippolyte Larzul <hippolarzul@gmail.com>
2024-03-12 19:17:06 +0100

    Testimonials

---
+++ src/components/One_Testimonial.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="bg-card rounded">
+    <div class="aurora-inner">
+      <b-container class="testimonials">
+        <b-row class="py-3">
+          <b-col cols="3" align-self="center" class="ms-2">
+            <img :src="imageSrc" :alt="name.split(' ')[0]" class="img-fluid rounded-circle border-0 night">
+          </b-col>
+          <b-col>
+            <h1 class="text-center">{{ name }}</h1>
+            <p class="m-2">{{ testimonialText }}</p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    testimonialText: {
+      type: String,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    }
+  },
+});
+</script>
+
+<style scoped>
+.bg-card {
+    background: linear-gradient(45deg, #32a6ff 0%, #3f6fff 49%, #54f585c4 82%);
+    background-size: 200%;
+    animation: aurora 10s infinite;
+}
+
+.aurora-inner {
+    background: radial-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.15));
+    background-size: 200%;
+    animation: aurora 7s infinite;
+}
+
+@keyframes aurora {
+    0% {
+        background-position: left top;
+    }
+    25% {
+        background-position: right top;
+    }
+    50% {
+        background-position: right bottom;
+    }
+    75% {
+        background-position: left bottom;
+    }
+    100% {
+        background-position: left top;
+    }
+}
+
+.night {
+    box-shadow: 0 0 0 0.2em #2626ad;
+}
+</style>
--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,13 +1,57 @@
 <template>
   <div>
-    <h1>Témoignages</h1>
+    <h1 class="text-center">Témoignages</h1>
+    <div>
+      <b-container class="testimonials">
+        <b-row>
+          <b-col v-for="(testimonial, index) in testimonials"
+              :key="index"
+              :cols="(index === testimonials.length - 1 && testimonials.length % 2 !== 0) ? 12 : 6"
+              :md="(index % 2 === 0) ? 6 : null"
+              class="my-3"
+              :class="{ 'offset-md-3': index === testimonials.length - 1 && testimonials.length % 2 !== 0 }"
+            >
+            <OneTestimonial
+        :name="testimonial.name"
+        :testimonialText="testimonial.testimonialText"
+        :imageSrc="testimonial.imageSrc"
+      />
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import OneTestimonial from './One_Testimonial.vue';
 
 export default defineComponent({
+  components: {
+    OneTestimonial,
+  },
+  data() {
+    return {
+      testimonials: [
+        {
+          name: 'Hippolyte Larzul',
+          testimonialText: 'Cette expérience avec les chiens de traîneau m\'a transporté dans un monde d\'aventure et de découvertes. Une expérience unique, entre complicité avec les animaux et exploration des paysages nordiques, à recommander à tous les passionnés d\'évasion.',
+          imageSrc: 'src/assets/images/testimonials/Hippo.png',
+        },
+        {
+          name: 'Héloïse Cagnard',
+          testimonialText: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          imageSrc: 'src/assets/images/testimonials/Helo.jpg',
+        },
+        {
+          name: 'Linard XXX',
+          testimonialText: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+          imageSrc: 'src/assets/images/testimonials/Linard.jpg',
+        },
+      ]
+    };
+  }
 });
 </script>
 
