d20a27209096a3fc6d96be61256bbc4ab3f106c6
Hippolyte Larzul <hippolarzul@gmail.com>
2024-03-18 21:27:57 +0100

    carousel experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,9 +1,104 @@
 <template>
   <div>
     <h1 class="text-center">Expérience</h1>
-    <p class="mx-2 text-center">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat ante sed magna ullamcorper, in varius dolor suscipit. Etiam semper velit non augue facilisis, in facilisis diam semper. Quisque sem nulla, interdum vitae euismod eget, aliquam et dolor. Etiam arcu quam, dapibus vel elit sit amet, fermentum volutpat sem. Quisque scelerisque id neque a rutrum. Ut ac ante ante. Vestibulum at elit sit amet lacus volutpat porttitor. Nulla pellentesque ipsum nisl, non varius sem dictum id. Praesent nec enim urna. Donec eu risus ac ante dictum commodo ac vel mi. Etiam orci nibh, congue sed neque quis, dapibus laoreet ante. Fusce ex arcu, convallis sed sapien ac, placerat pulvinar felis. Phasellus dignissim ligula magna, ac pulvinar risus lacinia venenatis.
-    </p>
+    <div>
+      <b-container class="testimonials mt-5">
+        <b-row>
+          <b-col cols="1"></b-col>
+          <b-col cols="4">
+            <div id="carouselIndicators" class="carousel slide">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="../assets/images/farm/1.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/2.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/3.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/4.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/5.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/6.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/7.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/8.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/9.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/10.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/11.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/12.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/13.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/14.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/15.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/16.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/17.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/18.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/19.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/20.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/21.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/22.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/23.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+                <div class="carousel-item">
+                  <img src="../assets/images/farm/24.jpg" class="d-block mx-auto img-fluid same-height">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </b-col>
+          <b-col>
+            <p class="mx-2 text-center">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat ante sed magna ullamcorper, in varius dolor suscipit. Etiam semper velit non augue facilisis, in facilisis diam semper. Quisque sem nulla, interdum vitae euismod eget, aliquam et dolor. Etiam arcu quam, dapibus vel elit sit amet, fermentum volutpat sem. Quisque scelerisque id neque a rutrum. Ut ac ante ante. Vestibulum at elit sit amet lacus volutpat porttitor. Nulla pellentesque ipsum nisl, non varius sem dictum id. Praesent nec enim urna. Donec eu risus ac ante dictum commodo ac vel mi. Etiam orci nibh, congue sed neque quis, dapibus laoreet ante. Fusce ex arcu, convallis sed sapien ac, placerat pulvinar felis. Phasellus dignissim ligula magna, ac pulvinar risus lacinia venenatis.
+            </p>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
   </div>
 </template>
 
@@ -15,4 +110,9 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.same-height {
+  height: 500px; 
+  object-fit: cover;
+  border-radius: 5%;
+}
 </style>
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
-      <h1 class="text-center">Introduction</h1>
-      <p class="mx-2 text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat ante sed magna ullamcorper, in varius dolor suscipit. Etiam semper velit non augue facilisis, in facilisis diam semper. Quisque sem nulla, interdum vitae euismod eget, aliquam et dolor. Etiam arcu quam, dapibus vel elit sit amet, fermentum volutpat sem. Quisque scelerisque id neque a rutrum. Ut ac ante ante. Vestibulum at elit sit amet lacus volutpat porttitor. Nulla pellentesque ipsum nisl, non varius sem dictum id. Praesent nec enim urna. Donec eu risus ac ante dictum commodo ac vel mi. Etiam orci nibh, congue sed neque quis, dapibus laoreet ante. Fusce ex arcu, convallis sed sapien ac, placerat pulvinar felis. Phasellus dignissim ligula magna, ac pulvinar risus lacinia venenatis.
-      </p>
+      <h1 class="text-center mb-3">Introduction</h1>
+      <div>
+        <b-container>
+          <b-row>
+            <b-col>
+              <p class="mx-2 text-center">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat ante sed magna ullamcorper, in varius dolor suscipit. Etiam semper velit non augue facilisis, in facilisis diam semper. Quisque sem nulla, interdum vitae euismod eget, aliquam et dolor. Etiam arcu quam, dapibus vel elit sit amet, fermentum volutpat sem. Quisque scelerisque id neque a rutrum. Ut ac ante ante. Vestibulum at elit sit amet lacus volutpat porttitor. Nulla pellentesque ipsum nisl, non varius sem dictum id. Praesent nec enim urna. Donec eu risus ac ante dictum commodo ac vel mi. Etiam orci nibh, congue sed neque quis, dapibus laoreet ante. Fusce ex arcu, convallis sed sapien ac, placerat pulvinar felis. Phasellus dignissim ligula magna, ac pulvinar risus lacinia venenatis.
+              </p>
+            </b-col>
+            <b-col>
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
     </div>
 </template>
   
