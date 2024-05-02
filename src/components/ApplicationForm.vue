a178642badd6da5b6ce5d63e3d05129483a41f73
Hippolyte Larzul <hippolarzul@gmail.com>
2024-05-02 22:44:19 +0200

    dates application form

--- src/components/ApplicationForm.vue
+++ src/components/ApplicationForm.vue
@@ -1,17 +1,30 @@
 <template>
-    <VDatePicker v-model.range="range" :disabled-dates="disabledDates" />
-  </template>
-  
+  <div id="ApplicationForm" class="mb-5">
+    <VDatePicker v-model.range="selectedRange" :disabled-dates="disabledDates" mode="date" />
+    <div v-if="selectedRange !== null">
+      <p>Dates sélectionnées :</p>
+      <ul>
+        <li>{{ formatDate(selectedRange.start) }} - {{ formatDate(selectedRange.end) }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { ref } from 'vue';
 
-const range = ref(null);
+const selectedRange = ref(null);
 const disabledDates = ref([]);
 
 const today = new Date();
-for (let i = 1; i <= 30; i++) {
+for (let i = 1; i <= 100; i++) {
   const date = new Date(today);
-  date.setDate(today.getDate() - i);
+  date.setDate(today.getDate() - i + 1);
   disabledDates.value.push(date);
 }
+
+const formatDate = (date) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('fr-FR', options);
+};
 </script>
